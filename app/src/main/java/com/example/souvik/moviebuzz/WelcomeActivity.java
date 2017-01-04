d47394144e7482a5df43c38f23340c3ef4ff2d94
gayensouvik1 <gayensouvik1@gmail.com>
2017-01-04 12:06:28 +0530

    welcome screen

---
+++ app/src/main/java/com/example/souvik/moviebuzz/WelcomeActivity.java
@@ -0,0 +1,41 @@
+package com.example.souvik.moviebuzz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    ImageView welcomeImage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        welcomeImage = (ImageView)findViewById(R.id.imageView);
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.welcome_animation);
+        welcomeImage.setAnimation(animation);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                finish();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
